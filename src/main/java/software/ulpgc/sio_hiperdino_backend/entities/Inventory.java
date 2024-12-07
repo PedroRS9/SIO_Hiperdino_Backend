5ea19cc7ecbae298f812fd61560b100007c34450
PedroRS9 <pedroglv98@gmail.com>
2024-12-07 01:40:41 +0000

    Added basic database tables

---
+++ src/main/java/software/ulpgc/sio_hiperdino_backend/entities/Inventory.java
@@ -0,0 +1,25 @@
+package software.ulpgc.sio_hiperdino_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+    private Integer quantity;
+}
